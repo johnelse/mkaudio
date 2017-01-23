463c73b762bcae3d6e5f4b539550d6251cffc227
John Else <john.else@gmail.com>
2017-01-23 17:35:48 +0000

    Add a help page
    Signed-off-by: John Else <john.else@gmail.com>

--- src/mkaudio.ml
+++ src/mkaudio.ml
@@ -1,1 +1,24 @@
-let () = ()
+open Cmdliner
+
+(* Command definitions *)
+let help_secs = [
+  `S "MORE HELP";
+  `P "Use `$(mname) $(i,command) --help' for help on a single command.";
+  `Noblank;
+]
+
+let default_cmd =
+  let doc = "mkaudio" in
+  let man = help_secs in
+  Term.(ret (pure (fun _ -> `Help (`Pager, None)) $ pure ())),
+  Term.info "mkaudio" ~version:"0.1" ~doc ~man
+
+let cmds = [
+]
+
+let () =
+  Printexc.record_backtrace true;
+  match Term.eval_choice default_cmd cmds with
+  | `Error _ -> exit 1
+  | `Version | `Help -> ()
+  | `Ok () -> ()
