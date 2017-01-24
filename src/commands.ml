fa1aad3b6a2684d3b13b17b3cc763244d0fbce04
John Else <john.else@gmail.com>
2017-01-24 11:28:40 +0000

    Implement sine command
    Signed-off-by: John Else <john.else@gmail.com>

---
+++ src/commands.ml
@@ -0,0 +1,37 @@
+let (>>=) value f =
+  match value with
+  | Result.Ok result -> f result
+  | Result.Error _ as error -> error
+
+let calculate_samples sample_rate duration tempo beats =
+  match duration, tempo, beats with
+  | Some duration, None, None ->
+    Result.Ok
+      ((float_of_int sample_rate) *. duration
+      |> int_of_float)
+  | None, Some tempo, Some beats ->
+    Result.Ok
+      ((float_of_int (sample_rate * beats * 60)) /. tempo
+      |> int_of_float)
+  | None, None, None
+  | Some _, _, _ ->
+    Result.Error
+      "You either need to specify the duration or the tempo and number of beats"
+  | None, _, _ ->
+    Result.Error "You need to specify both the tempo and number of beats"
+
+let sine channels sample_rate duration tempo beats frequency output_file =
+  calculate_samples sample_rate duration tempo beats
+  >>= fun samples ->
+    let wav =
+      new Audio.IO.Writer.to_wav_file channels sample_rate output_file in
+    let buffer_length = 1024 in
+    let buffer = Audio.create channels buffer_length in
+    let sine =
+      new Audio.Generator.of_mono
+        (new Audio.Mono.Generator.sine sample_rate frequency) in
+    for i = 0 to samples / buffer_length - 1 do
+      sine#fill buffer 0 buffer_length;
+      wav#write buffer 0 buffer_length;
+    done;
+    Result.Ok (wav#close)
--- src/mkaudio.ml
+++ src/mkaudio.ml
@@ -1,24 +1,103 @@
 open Cmdliner
 
-(* Command definitions *)
+(* man page definition. *)
+let help man_format cmds topic =
+  match topic with
+  | None -> `Help (`Pager, None)
+  | Some topic ->
+    let topics = "topics" :: cmds in
+    let conv, _ = Arg.enum (List.rev_map (fun s -> (s, s)) topics) in
+    match conv topic with
+    | `Error e -> `Error (false, e)
+    | `Ok t when t = "topics" ->
+      List.iter print_endline topics;
+      `Ok (Result.Ok ())
+    | `Ok t when List.mem t cmds -> `Help (man_format, Some t)
+    | `Ok t ->
+      let page = (topic, 7, "", "", ""), [`S topic; `P "Say something"] in
+      Manpage.print man_format Format.std_formatter page;
+      `Ok (Result.Ok ())
+
 let help_secs = [
   `S "MORE HELP";
   `P "Use `$(mname) $(i,command) --help' for help on a single command.";
   `Noblank;
 ]
 
-let default_cmd =
+let help_cmd =
+  let topic =
+    let doc = "The topic to get help on. `topics' lists the topics." in
+    Arg.(value & pos 0 (some string) None & info [] ~docv:"TOPIC" ~doc)
+  in
+  let doc = "display help about mkaudio" in
+  let man = [
+    `S "DESCRIPTION";
+    `P "Prints help about mkaudio commands."
+  ] @ help_secs in
+  Term.(ret (pure help $ Term.man_format $ Term.choice_names $ topic)),
+  Term.info "help" ~doc ~man
+
+(* Command definitions. *)
+let channels =
+  let doc = "The number of channels to use when creating the audio file." in
+  Arg.(value & opt int 2 & info ["channels"] ~docv:"CHANNELS" ~doc)
+
+let sample_rate =
+  let doc = "The sample rate to use when creating the audio file." in
+  Arg.(value & opt int 44100 & info ["samplerate"] ~docv:"SAMPLERATE" ~doc)
+
+let duration =
+  let doc = "The duration of the created file in seconds." in
+  Arg.(value & opt (some float) None & info ["duration"] ~docv:"DURATION" ~doc)
+
+let tempo =
+  let doc = "The tempo of the created file in beats per minute." in
+  Arg.(value & opt (some float) None & info ["tempo"] ~docv:"TEMPO" ~doc)
+
+let beats =
+  let doc = "The duration of the created file in beats." in
+  Arg.(value & opt (some int) None & info ["beats"] ~docv:"BEATS" ~doc)
+
+let frequency =
+  let doc = "The frequency of the generated sound" in
+  Arg.(value & opt float 440. & info ["frequency"] ~docv:"FREQUENCY" ~doc)
+
+let output_file =
+  let doc = "The file to write." in
+  Arg.(required & pos 0 (some string) None & info [] ~docv:"OUTPUTFILE" ~doc)
+
+let sine_cmd =
+  let doc = "write an audio file containing a sine wave." in
+  let man = [
+    `S "DESCRIPTION";
+    `P "Write an audio file containing a sine wave.";
+  ] @ help_secs in
+  Term.(pure Commands.sine
+    $ channels
+    $ sample_rate
+    $ duration
+    $ tempo
+    $ beats
+    $ frequency
+    $ output_file),
+  Term.info "sine" ~doc ~man
+
+let default_command =
   let doc = "mkaudio" in
   let man = help_secs in
   Term.(ret (pure (fun _ -> `Help (`Pager, None)) $ pure ())),
   Term.info "mkaudio" ~version:"0.1" ~doc ~man
 
-let cmds = [
+let commands = [
+  help_cmd;
+  sine_cmd;
 ]
 
 let () =
   Printexc.record_backtrace true;
-  match Term.eval_choice default_cmd cmds with
+  match Term.eval_choice default_command commands with
   | `Error _ -> exit 1
-  | `Version | `Help -> ()
-  | `Ok () -> ()
+  | `Ok (Result.Ok ()) | `Version | `Help -> ()
+  | `Ok (Result.Error msg) ->
+    print_endline msg;
+    exit 1
