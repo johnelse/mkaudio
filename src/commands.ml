cf515c5afec0846ee2e1f44925b0e54f509d0ce5
John Else <john.else@gmail.com>
2017-02-13 01:41:54 +0000

    Pull common logic out of sine command
    Signed-off-by: John Else <john.else@gmail.com>

--- src/commands.ml
+++ src/commands.ml
@@ -20,18 +20,20 @@ let calculate_samples sample_rate duration tempo beats =
   | None, _, _ ->
     Result.Error "You need to specify both the tempo and number of beats"
 
+let write_wav channels sample_rate samples generator output_file =
+  let wav = new Audio.IO.Writer.to_wav_file channels sample_rate output_file in
+  let buffer_length = 1024 in
+  let buffer = Audio.create channels buffer_length in
+  for i = 0 to samples / buffer_length - 1 do
+    generator#fill buffer 0 buffer_length;
+    wav#write buffer 0 buffer_length
+  done;
+  Result.Ok (wav#close)
+
 let sine channels sample_rate duration tempo beats frequency output_file =
   calculate_samples sample_rate duration tempo beats
   >>= fun samples ->
-    let wav =
-      new Audio.IO.Writer.to_wav_file channels sample_rate output_file in
-    let buffer_length = 1024 in
-    let buffer = Audio.create channels buffer_length in
-    let sine =
+    let generator =
       new Audio.Generator.of_mono
         (new Audio.Mono.Generator.sine sample_rate frequency) in
-    for i = 0 to samples / buffer_length - 1 do
-      sine#fill buffer 0 buffer_length;
-      wav#write buffer 0 buffer_length;
-    done;
-    Result.Ok (wav#close)
+    write_wav channels sample_rate samples generator output_file
