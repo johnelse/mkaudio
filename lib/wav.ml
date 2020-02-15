19f6df09a0b19feeb3982ab0ca1ee47425de0b75
John Else <john.else@gmail.com>
2020-02-15 00:52:50 +0000

    Write the correct number of samples for sine/square/triangle/white noise
    Previously we would only ever write a number of samples which was a
    multiple of the buffer size (1024).

--- lib/wav.ml
+++ lib/wav.ml
@@ -2,8 +2,15 @@ let write ~channels ~sample_rate ~samples ~generator ~output_file =
   let wav = new Audio.IO.Writer.to_wav_file channels sample_rate output_file in
   let buffer_length = 1024 in
   let buffer = Audio.create channels buffer_length in
-  for _ = 0 to samples / buffer_length - 1 do
-    generator#fill buffer 0 buffer_length;
-    wav#write buffer 0 buffer_length
-  done;
+
+  let rec write samples_left =
+    let to_write = min samples_left buffer_length in
+    if to_write > 0 then begin
+      generator#fill buffer 0 to_write;
+      wav#write buffer 0 to_write;
+      write (samples_left - to_write)
+    end
+  in
+
+  write samples;
   Result.Ok (wav#close)
