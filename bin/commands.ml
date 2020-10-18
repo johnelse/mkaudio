ed74bcbf12aca2a3b42f6af0d5555702a5b6a40f
John Else <john.else@gmail.com>
2020-10-18 22:04:59 +0100

    Improve performance when generating multiple repeats of a beat
    Instead of generating the beat each time, generate a buffer containing
    one repeat of the beat once, and write this buffer to the output file
    multiple times.

--- bin/commands.ml
+++ bin/commands.ml
@@ -56,6 +56,39 @@ let white_noise channels sample_rate gain duration tempo beats output_file =
         (new Audio.Mono.Generator.white_noise ~volume:gain sample_rate) in
     Wav.write ~channels ~sample_rate ~samples ~generator ~output_file
 
+let make_beat channels sample_rate gain samples steps =
+  let step_length = samples / (List.length steps) in
+  let step_buffer = Audio.create channels step_length in
+  let beat_buffer = Audio.create channels samples in
+  let rec add_steps offset = function
+    | [] -> ()
+    | beat :: rest -> begin
+      Audio.clear step_buffer;
+
+      if beat.Beat.kick
+      then begin
+        let kick = Generators.kick ~sample_rate ~gain in
+        kick#fill_add step_buffer
+      end;
+      if beat.Beat.snare
+      then begin
+        let snare = Generators.snare ~sample_rate ~gain in
+        snare#fill_add step_buffer
+      end;
+      if beat.Beat.hihat
+      then begin
+        let hihat = Generators.hihat ~sample_rate ~gain in
+        hihat#fill_add step_buffer
+      end;
+
+      let target_buffer = Audio.sub beat_buffer offset step_length in
+      Audio.blit step_buffer target_buffer;
+      add_steps (offset + step_length) rest
+    end
+  in
+  add_steps 0 steps;
+  beat_buffer
+
 let beat channels sample_rate gain tempo kick snare hihat repeats output_file =
   let repeats = max 1 repeats in
   Beat.parse_patterns ~kick ~snare ~hihat
@@ -63,33 +96,9 @@ let beat channels sample_rate gain tempo kick snare hihat repeats output_file =
     Time.calculate_samples
       ~sample_rate ~duration:None ~tempo ~steps:(Some (List.length steps))
   >>= fun samples ->
-    let step_length = samples / (List.length steps) in
-    let buffer = Audio.create channels step_length in
+    let beat_buffer = make_beat channels sample_rate gain samples steps in
     let wav = new Audio.IO.Writer.to_wav_file channels sample_rate output_file in
-    let rec add_steps buffer = function
-      | [] -> ()
-      | beat :: rest -> begin
-        Audio.clear buffer;
-        if beat.Beat.kick
-        then begin
-          let kick = Generators.kick ~sample_rate ~gain in
-          kick#fill_add buffer
-        end;
-        if beat.Beat.snare
-        then begin
-          let snare = Generators.snare ~sample_rate ~gain in
-          snare#fill_add buffer
-        end;
-        if beat.Beat.hihat
-        then begin
-          let hihat = Generators.hihat ~sample_rate ~gain in
-          hihat#fill_add buffer
-        end;
-        wav#write buffer;
-        add_steps buffer rest
-      end
-    in
     for _ = 1 to repeats do
-      add_steps buffer steps
+      wav#write beat_buffer
     done;
     Result.Ok (wav#close)
