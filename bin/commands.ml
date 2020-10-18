3cf5e9e8812411d801469b4ec949e07aa84b5b7c
John Else <john.else@gmail.com>
2020-10-18 21:14:05 +0100

    Pull beat generators out into separate module

--- bin/commands.ml
+++ bin/commands.ml
@@ -56,33 +56,6 @@ let white_noise channels sample_rate gain duration tempo beats output_file =
         (new Audio.Mono.Generator.white_noise ~volume:gain sample_rate) in
     Wav.write ~channels ~sample_rate ~samples ~generator ~output_file
 
-let kick_gen sample_rate gain =
-  let adsr = Audio.Mono.Effect.ADSR.make sample_rate (0.001, 0.3, 0., 1.) in
-  let sine =
-    new Audio.Mono.Generator.sine ~volume:(0.4 *. gain) sample_rate 60. in
-  let kick = new Audio.Mono.Generator.adsr adsr sine in
-  new Audio.Generator.of_mono kick
-
-let snare_gen sample_rate gain =
-  let lpf =
-    new Audio.Mono.Effect.biquad_filter sample_rate `Low_pass 2000. 2. in
-  let adsr = Audio.Mono.Effect.ADSR.make sample_rate (0., 0.08, 0., 1.) in
-  let noise =
-    new Audio.Mono.Generator.white_noise ~volume:(0.3 *. gain) sample_rate in
-  let filtered = new Audio.Mono.Generator.chain noise lpf in
-  let snare = new Audio.Mono.Generator.adsr adsr filtered in
-  new Audio.Generator.of_mono snare
-
-let hihat_gen sample_rate gain =
-  let hpf =
-    new Audio.Mono.Effect.biquad_filter sample_rate `High_pass 8000. 2. in
-  let adsr = Audio.Mono.Effect.ADSR.make sample_rate (0., 0.05, 0., 1.) in
-  let noise =
-    new Audio.Mono.Generator.white_noise ~volume:(0.3 *. gain) sample_rate in
-  let filtered = new Audio.Mono.Generator.chain noise hpf in
-  let snare = new Audio.Mono.Generator.adsr adsr filtered in
-  new Audio.Generator.of_mono snare
-
 let beat channels sample_rate gain tempo kick snare hihat repeats output_file =
   let repeats = max 1 repeats in
   Beat.parse_patterns ~kick ~snare ~hihat
@@ -99,18 +72,18 @@ let beat channels sample_rate gain tempo kick snare hihat repeats output_file =
         Audio.clear buffer;
         if beat.Beat.kick
         then begin
-          let kick_gen' = kick_gen sample_rate gain in
-          kick_gen'#fill_add buffer
+          let kick = Generators.kick sample_rate gain in
+          kick#fill_add buffer
         end;
         if beat.Beat.snare
         then begin
-          let snare_gen' = snare_gen sample_rate gain in
-          snare_gen'#fill_add buffer
+          let snare = Generators.snare sample_rate gain in
+          snare#fill_add buffer
         end;
         if beat.Beat.hihat
         then begin
-          let hihat_gen' = hihat_gen sample_rate gain in
-          hihat_gen'#fill_add buffer
+          let hihat = Generators.hihat sample_rate gain in
+          hihat#fill_add buffer
         end;
         wav#write buffer;
         add_steps buffer (offset + 1) rest
---
+++ lib/generators.mli
@@ -0,0 +1,5 @@
+val kick : int -> float -> Audio.Generator.of_mono
+
+val snare : int -> float -> Audio.Generator.of_mono
+
+val hihat : int -> float -> Audio.Generator.of_mono
